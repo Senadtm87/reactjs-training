3254c7e00f1c640a803f56a0e8094ef50139a3dd
Senad Mulaosmanovic <senad_tm87@yahoo.com>
2020-04-24 14:51:05 +0200

    Updates

---
+++ precache-manifest.ed92b5c3c807e07d4b4e3adf866d14bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd0883d25477649d1497fdeb759482fe",
+    "url": "/reactjs-training/index.html"
+  },
+  {
+    "revision": "f6bd64b53f876653bfa7",
+    "url": "/reactjs-training/static/css/main.8cb61e0f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/reactjs-training/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs-training/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6bd64b53f876653bfa7",
+    "url": "/reactjs-training/static/js/main.f74d5e94.chunk.js"
+  },
+  {
+    "revision": "0dc80bb39eba87031638",
+    "url": "/reactjs-training/static/js/runtime-main.77ab3d61.js"
+  }
+]);
